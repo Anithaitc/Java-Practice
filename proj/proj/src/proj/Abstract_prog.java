2d077e89c1baab1bc638327a0e7b7cf6f03b7432
Anitha <106381826+Anithaitc@users.noreply.github.com>
2022-07-11 09:41:38 +0530

    Delete proj/proj directory

--- proj/proj/src/proj/Abstract_prog.java
+++
@@ -1,8 +0,0 @@
-package proj;
-
-public class Abstract_prog {
-	Shape s1= new Circle("Yellow",2.2);
-	Shape s2= new Rectangle("Red",2,4);
-	System.out.println(s1.toString());
-	System.out.println(s2.toString());
-}
